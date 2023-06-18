3c8daa482215eaf25166f33b4ace41f4fd8d05c9
akatukam <123602031+akatukam@users.noreply.github.com>
2023-06-18 01:16:53 -0400

    Update skip_file.py

--- skip_file.py
+++ skip_file.py
@@ -14,26 +14,27 @@ def git_top_dir() -> str:
 # Get the top-level directory of the Git repository
 folder_path = git_top_dir()
 
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        if file == '__init__.py':
-            file_path = os.path.join(root, file)
-            print(f"Processing file: {file_path}")
-
-            with open(file_path, 'r+') as f:
-                content = f.read()
-                print(f"Original content: {content}")
-
-                if '# pytype: skip-file' not in content:
+if __name__ == '__main__':  
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file == '__init__.py':
+                file_path = os.path.join(root, file)
+                print(f"Processing file: {file_path}")
+    
+                with open(file_path, 'r+') as f:
+                    content = f.read()
+                    print(f"Original content: {content}")
+    
+                    if '# pytype: skip-file' not in content:
+                        f.seek(0, 0)
+                        f.write('# pytype: skip-file\n' + content)
+                        print("Skip-file added successfully")
+                    else:
+                        print("Skip-file already exists")
+    
+                with open(file_path, 'r+') as f:    
                     f.seek(0, 0)
-                    f.write('# pytype: skip-file\n' + content)
-                    print("Skip-file added successfully")
-                else:
-                    print("Skip-file already exists")
-
-            with open(file_path, 'r+') as f:    
-                f.seek(0, 0)
-                modified_content = f.read()
-                print(f"Modified content: {modified_content}")
+                    modified_content = f.read()
+                    print(f"Modified content: {modified_content}")
 
 print("Adding skip-file was successful")
