3a9d95e3c9e05c76dbcb31ee3cd4427a4e7a9b3d
akatukam <123602031+akatukam@users.noreply.github.com>
2023-06-19 01:31:41 -0400

    Update region_test.py

--- region_test.py
+++ region_test.py
@@ -21,76 +21,16 @@ def git_top_dir() -> str:
 folder_path = git_top_dir()
 
 def check_method_signature(module, method_name):
-    """ Accepts a module and a method/function from that module and
-        returns true if the method signature either doesn't contain 
-        a riff or "region" at all, or contains "region" exactly. Else
-        it returns false.
-        :type module: object
-        :param module: The module being checked.
-        :type method_name: string
-        :param method_name: Name of the method to check.
-    """
     return True
-"""
-    print("hello")
-    print("module:")
-    print(type(module))
-    module_name = module.name
-    print(f"Module name: {module_name}")
-    module_act = importlib.util.module_from_spec(module)
-    print(type(module_act))
-"""
-"""      
-    method = getattr(module, method_name, None)
-    print("method:")
-    print(type(method))
-    method_name = method.__name__
-    print(f"Method name: {method_name}")
-"""
-"""
-    method = getattr(module, method_name, None)
-    if method is not None:
-        (source_lines, _) = inspect.getsourcelines(method)
-        # partial matches with "egion" to see if any riff is present
-        if re.search(r"egion", source_lines[0]):
-            # checks if that riff is "region" exactly
-            pattern = r"def {}\(.*\bregion(\s|:|\)).*:".format(re.escape(method_name))
-            return bool(re.findall(pattern, source_lines[0]))
-        else:
-            return True
-    else:
-        return True
-"""
+
 def check_module_methods(module):
-    """ Accepts a module and calls check_method_signature on each 
-        function/method present in it.
-        :type module: object
-        :param module: The module being checked.
-    """
-
     module_act = importlib.util.module_from_spec(module)
-    print("module:")
-    print(module_act.__name__)
-
     module_source = inspect.getsource(module_act)
-    print("module_source:")
-    print(module_source)
-
-    
-    """
-    has_region = True
-    for (name, member) in inspect.getmembers(module):
-        print("inspect get members module:")
-        print(inspect.getmembers(module))
-        if inspect.isfunction(member) or inspect.ismethod(member):
-            print("name:")
-            print(name)
-            if not check_method_signature(module, name):
-               has_region = False
-    if not has_region:
-        print(f"Assertion failed: Module: {module.name}")
-        assert has_region
-    """
+    method_matches = re.findall(r"def(.*)?:", module_source)
+    for method_match in method_matches:
+        method_name = re.findall(r"(\w+)\s*\(", method_match)
+        print(method_match)
+        print(method_name)
 
 if __name__ == '__main__':   
     """  
