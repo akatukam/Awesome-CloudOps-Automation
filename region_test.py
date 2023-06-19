c5e335ab8147cf7a60e4bcc53168e28599385e48
akatukam <123602031+akatukam@users.noreply.github.com>
2023-06-19 03:34:39 -0400

    Update region_test.py

--- region_test.py
+++ region_test.py
@@ -35,8 +35,7 @@ def check_module_methods(module):
     method_matches = re.findall(r"def (.*?)\)", module_source, flags=re.DOTALL)
     for method_match in method_matches:
         method_name = re.findall(r"(\w+)\s*\(", method_match)
-        method_match_new = method_match.replace(method_name[0], "")
-        if not check_method_signature_two(method_match_new, method_name[0]):
+        if not check_method_signature_two(method_match, method_name[0]):
             has_region = False
     return has_region
 
