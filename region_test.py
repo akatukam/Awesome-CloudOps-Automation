5632b1c02e4a47e10cee3ad27f3dcec78397ba4d
akatukam <123602031+akatukam@users.noreply.github.com>
2023-06-19 03:05:46 -0400

    Update region_test.py

--- region_test.py
+++ region_test.py
@@ -23,7 +23,7 @@ folder_path = git_top_dir()
 def check_method_signature(param, method_name):
     if re.search(r"egion", param):
         # checks if that riff is "region" exactly
-        pattern = r"(?<![^\s(])region(?=\s|:|\))"
+        pattern = r"(?<![^\s(,])region(?=\s|:|\))"
         return bool(re.findall(pattern, param+")"))
     else:
         return True
