69f9cc25c8a0f9722114ac633ab216746a73a276
akatukam <123602031+akatukam@users.noreply.github.com>
2023-06-19 03:15:41 -0400

    Update region_test.py

--- region_test.py
+++ region_test.py
@@ -24,6 +24,9 @@ def check_method_signature(param, method_name):
     if re.search(r"egion", param):
         # checks if that riff is "region" exactly
         pattern = r"(?<![^\s(,])region(?=\s|:|\))"
+        if not bool(re.findall(pattern, param+")")):
+            print(param)
+            print(method_name)
         return bool(re.findall(pattern, param+")"))
     else:
         return True
